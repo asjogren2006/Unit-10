16dfe30aa0d6f3c8570f57b51c4af9720dfa1daf
Andrew <asjogren@leisdstudent.ws>
2022-03-22 10:58:22 -0500

    newest

--- script.js
+++ script.js
@@ -3,10 +3,39 @@ function askname() {
     return questionname
 }
 
-alert(`Hello, ${askname()}!`)
+function sayhi(x) {
+    alert(`Hello, ${x}`)
+}
+
+sayhi(askname())
 
 function asktemp() {
     var questiontemp = prompt("What is the temperature?")
     return questiontemp
 }
 
+
+function choose() {
+    var choose = prompt("F for fahrenheit to celsius, C for celsius to fahrenheit.")
+    return choose
+}
+
+// choose()
+
+if (choose == "F") {
+    function ctof(temp1) {
+        var math1 = ((temp1-32)*5/9)
+        return math1
+        console.log(ctof(asktemp()))
+    }
+}
+
+console.log(ctof(asktemp()))
+
+
+// function ctof() {
+//     if (choose == "F") {
+//         var math1 = ((questiontemp+32)*5/9)
+//     }
+//     return math1
+// }
