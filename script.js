8a001f91e31a7c8ee106dbde342d0198688fe884
Andrew <asjogren@leisdstudent.ws>
2022-03-24 10:50:54 -0500

    Final version

--- script.js
+++ script.js
@@ -14,45 +14,67 @@ function asktemp() {
     return questiontemp
 }
 
+function question1() {
+    var imp = prompt(`Press "F" for fahrentheit to celsius, or "C" for celsius to fahrenheit.`)
+    return imp
+}
+
+
+// f to c
+
+function domath1(z) {
+  var math1 = ((z-32)*5/9)
+  return math1
+}
+
+// c to f
+
+function domath2(c) {
+  var math2 = ((c*9)/5+32)
+  return math2
+}
+
+function ctof(m) {
+   alert(`That is ${m} degrees fahrenheit.`)
+}
+
+function finish() {
+
+    var q = question1()
+
+    if (q == "C") {
+        ctof(domath2(asktemp()))
+    } else (q == "F") 
+        ctof(domath1(asktemp()))
+    }
+
+
+
+
+finish()
 // function domath() {
 
 // }
 
 
-function bigquestion(p, l) {
-    var choose = prompt("F for fahrenheit to celsius, C for celsius to fahrenheit.")
+// function bigquestion(p, l) {
+//     var choose = prompt("F for fahrenheit to celsius, C for celsius to fahrenheit.")
     
 
 
-        if (choose == "F")   
-            var math1 = ((asktemp()-32)*5/9)
-            alert(`That is ${math1} degrees fahrenheit.`)
+//         if (choose == "F") {
+//             var math1 = ((asktemp()-32)*5/9)
+//         }
         
-    
+//        if (choose == "C") {
+//            var math2 = ((asktemp()*9)/5+32)
+//         }
+        
+       
+       
 
             
-}   
+// }   
 
 
-bigquestion()
-
-
-
-
-
-
-
-
-
-
-
-// function ctof(temp1) {
-//     var math1 = ((temp1-32)*5/9)
-//     return math1
-// }
-
-// function saystatement(y) {
-//     alert(`That is ${y} degrees fahrenheit.`)
-// }
-
-// saystatement(ctof(asktemp()))
+// bigquestion()
